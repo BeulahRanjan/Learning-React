7b95560f9e873abe7af20106aad05d3a8b19a510
Beulah Ranjan <beulahranjan@gmail.com>
2026-02-16 16:57:11 +0530

    Updated

--- src/Chapter-2- React with JSX.jsx
+++ src/Chapter-2- React with JSX.jsx
@@ -167,3 +167,14 @@ Updates only changed modules
 No full page reload
 
 Preserves application state */
+
+
+/*
+================
+Webpack Loaders
+================
+
+Loaders in Webpack are functions that transform source code during the build process. 
+They allow Webpack to handle files that browsers cannot read natively, such as ES6, JSX, and SCSS. Loaders like babel-loader transpile modern 
+JavaScript and JSX into browser-compatible JavaScript, while styling loaders such as css-loader process CSS/SCSS files and bundle styles directly into JavaScript. 
+This eliminates the need for separate <link> tags for stylesheets. */
