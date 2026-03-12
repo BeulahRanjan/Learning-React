00a7a5d557f9adc246b7c8427b5c351f6337f65c
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-12 17:05:15 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Chapter1 from './Chapter-1-Pure React.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Chapter1 />
+    
   </StrictMode>,
 )
