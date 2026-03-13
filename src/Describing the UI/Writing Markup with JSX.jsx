ff4211888f6edf6d8e33f175822dc8f9c302239e
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-13 12:00:40 +0530

    Updated

--- src/Describing the UI/Writing Markup with JSX.jsx
+++ src/Describing the UI/Writing Markup with JSX.jsx
@@ -4,5 +4,20 @@ JSX is a syntax extension for JavaScript that lets you write HTML-like
   components, most React developers prefer the conciseness of JSX, and 
   most codebases use it.
 
-  
+  The Web has been built on HTML, CSS, and JavaScript. For many years, web developers kept content in HTML, design in CSS, and logic in JavaScript—often in separate files! Content was marked up inside HTML while the page’s logic lived separately in JavaScript:
+
+HTML markup with purple background and a div with two child tags: p and form. 
+HTML
+
+Three JavaScript handlers with yellow background: onSubmit, onLogin, and onClick.
+JavaScript
+
+But as the Web became more interactive, logic increasingly determined content. JavaScript was in charge of the HTML! This is why in React, rendering logic and markup live together in the same place—components.
+
+Keeping a button’s rendering logic and markup together ensures that they stay in sync with each other on every edit. Conversely, details that are unrelated, such as the button’s markup and a sidebar’s markup, are isolated from each other, making it safer to change either of them on their own.
+
+Each React component is a JavaScript function that may contain some markup that React renders into the browser. React components use a syntax extension called JSX to represent that markup. JSX looks a lot like HTML, but it is a bit stricter and can display dynamic information. The best way to understand this is to convert some HTML markup to JSX markup.
+
+Note:
+JSX and React are two separate things. They’re often used together, but you can use them independently of each other. JSX is a syntax extension, while React is a JavaScript library.
  */
