eec92254243bac2dd41c092ebe20df2c4f475227
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-17 20:08:29 +0530

    Updated

--- src/Describing the UI/4 - JavaScript in JSX with Curly Braces.jsx
+++ src/Describing the UI/4 - JavaScript in JSX with Curly Braces.jsx
@@ -27,5 +27,31 @@ export default function Avatar() {
   );
 }
 
-Notice the difference between className="avatar", which specifies an "avatar" CSS class name that makes the image round, and src={avatar} that reads the value of the JavaScript variable called avatar. That’s because curly braces let you work with JavaScript right there in your markup!
+Notice the difference between className="avatar", which specifies an 
+"avatar" CSS class name that makes the image round, and src={avatar} 
+that reads the value of the JavaScript variable called avatar. 
+That’s because curly braces let you work with JavaScript right there in 
+your markup!
+
+
+
+Using curly braces: A window into the JavaScript world 
+JSX is a special way of writing JavaScript. That means it’s possible to use JavaScript inside it—with curly braces { }. The example below first declares a name for the scientist, name, then embeds it with curly braces inside the <h1>:
+export default function TodoList() {
+  const name = 'Gregorio Y. Zara';
+  return (
+    <h1>{name}'s To Do List</h1>
+  );
+}
+
+Any JavaScript expression will work between curly braces, including function calls like formatDate():
+
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
  */
