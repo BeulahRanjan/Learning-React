ee84d42162a5c356054775836c7699763388e63f
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-18 11:02:11 +0530

    Updated

--- src/Describing the UI/5 - Passing Props to a Component.jsx
+++ src/Describing the UI/5 - Passing Props to a Component.jsx
@@ -25,5 +25,69 @@ export default function Profile() {
 }
 
 
+Step 1: Pass props to the child component 
+First, pass some props to Avatar. For example, let’s pass two props: person (an object), and size (a number):
 
- */
+export default function Profile() {
+  return (
+    <Avatar
+      person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }}
+      size={100}
+    />
+  );
+}
+  Now you can read these props inside the Avatar component.
+
+  Step 2: Read props inside the child component 
+You can read these props by listing their names person, size separated by the commas inside ({ and }) directly after function Avatar. This lets you use them inside the Avatar code, like you would with a variable.
+
+function Avatar({ person, size }) {
+  // person and size are available here
+}
+Add some logic to Avatar that uses the person and size props for rendering, and you’re done.
+
+Now you can configure Avatar to render in many different ways with different props. Try tweaking the values!
+ 
+import { getImageUrl } from './utils.js';
+
+function Avatar({ person, size }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+export default function Profile() {
+  return (
+    <div>
+      <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi', 
+          imageId: 'YfeOqp2'
+        }}
+      />
+      <Avatar
+        size={80}
+        person={{
+          name: 'Aklilu Lemma', 
+          imageId: 'OKS67lh'
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{ 
+          name: 'Lin Lanying',
+          imageId: '1bX5QH6'
+        }}
+      />
+    </div>
+  );
+}
+
+*/
