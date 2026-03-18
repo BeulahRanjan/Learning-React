e38eb38d0e0e8bd041d8bf897182e8b91c0fd33d
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-18 11:44:38 +0530

    Updated

--- src/Describing the UI/5 - Passing Props to a Component.jsx
+++ src/Describing the UI/5 - Passing Props to a Component.jsx
@@ -183,4 +183,24 @@ export default function Profile() {
 Try replacing the <Avatar> inside <Card> with some text to see how the Card component can wrap any nested content. It doesn’t need to “know” what’s being rendered inside of it. You will see this flexible pattern in many places.
 
 You can think of a component with a children prop as having a “hole” that can be “filled in” by its parent components with arbitrary JSX. You will often use the children prop for visual wrappers: panels, grids, etc.
+
+How props change over time 
+The Clock component below receives two props from its parent component: color and time. (The parent component’s code is omitted because it uses state, which we won’t dive into just yet.)
+
+Try changing the color in the select box below:
+
+
+export default function Clock({ color, time }) {
+  return (
+    <h1 style={{ color: color }}>
+      {time}
+    </h1>
+  );
+}
+
+This example illustrates that a component may receive different props over time. Props are not always static! Here, the time prop changes every second, and the color prop changes when you select another color. Props reflect a component’s data at any point in time, rather than only in the beginning.
+
+However, props are immutable—a term from computer science meaning “unchangeable”. When a component needs to change its props (for example, in response to a user interaction or new data), it will have to “ask” its parent component to pass it different props—a new object! Its old props will then be cast aside, and eventually the JavaScript engine will reclaim the memory taken by them.
+
+Don’t try to “change props”. When you need to respond to the user input (like changing the selected color), you will need to “set state”.
 */
