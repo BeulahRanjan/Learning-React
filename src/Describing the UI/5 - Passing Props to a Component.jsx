1cc30e00be6c7ed92f740d0c8c180deecbc6d7aa
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-18 11:39:18 +0530

    Updated

--- src/Describing the UI/5 - Passing Props to a Component.jsx
+++ src/Describing the UI/5 - Passing Props to a Component.jsx
@@ -112,4 +112,75 @@ Now, if <Avatar person={...} /> is rendered with no size prop, the size will be 
 
 The default value is only used if the size prop is missing or if you pass size={undefined}. But if you pass size={null} or size={0}, the default value will not be used.
 
+Forwarding props with the JSX spread syntax
+
+Sometimes, passing props gets very repetitive:
+
+function Profile({ person, size, isSepia, thickBorder }) {
+  return (
+    <div className="card">
+      <Avatar
+        person={person}
+        size={size}
+        isSepia={isSepia}
+        thickBorder={thickBorder}
+      />
+    </div>
+  );
+}
+There’s nothing wrong with repetitive code—it can be more legible. But at times you may value conciseness. Some components forward all of their props to their children, like how this Profile does with Avatar. Because they don’t use any of their props directly, it can make sense to use a more concise “spread” syntax:
+
+function Profile(props) {
+  return (
+    <div className="card">
+      <Avatar {...props} />
+    </div>
+  );
+}
+This forwards all of Profile’s props to the Avatar without listing each of their names.
+
+Use spread syntax with restraint. 
+If you’re using it in every other component, something is wrong. 
+Often, it indicates that you should split your components and pass 
+children as JSX. More on that next!
+
+Passing JSX as children 
+It is common to nest built-in browser tags:
+
+<div>
+  <img />
+</div>
+Sometimes you’ll want to nest your own components the same way:
+
+<Card>
+  <Avatar />
+</Card>
+When you nest content inside a JSX tag, the parent component will receive that content in a prop called children. For example, the Card component below will receive a children prop set to <Avatar /> and render it in a wrapper div:
+import Avatar from './Avatar.js';
+
+function Card({ children }) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
+}
+
+export default function Profile() {
+  return (
+    <Card>
+      <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2'
+        }}
+      />
+    </Card>
+  );
+}
+
+Try replacing the <Avatar> inside <Card> with some text to see how the Card component can wrap any nested content. It doesn’t need to “know” what’s being rendered inside of it. You will see this flexible pattern in many places.
+
+You can think of a component with a children prop as having a “hole” that can be “filled in” by its parent components with arbitrary JSX. You will often use the children prop for visual wrappers: panels, grids, etc.
 */
