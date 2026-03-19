95ade20e62d32e97b549c658453a497802fb63e1
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-19 14:06:24 +0530

    Updated

--- src/Describing the UI/7 - Rendering Lists.jsx
+++ src/Describing the UI/7 - Rendering Lists.jsx
@@ -1,3 +1,28 @@
 /*
+Rendering data from arrays 
 
+Say that you have a list of content.
+<ul>
+  <li>Creola Katherine Johnson: mathematician</li>
+  <li>Mario José Molina-Pasquel Henríquez: chemist</li>
+  <li>Mohammad Abdus Salam: physicist</li>
+  <li>Percy Lavon Julian: chemist</li>
+  <li>Subrahmanyan Chandrasekhar: astrophysicist</li>
+</ul>
+The only difference among those list items is their contents, their data. You will often need to show several instances of the same component using different data when building interfaces: from lists of comments to galleries of profile images. In these situations, you can store that data in JavaScript objects and arrays and use methods like map() and filter() to render lists of components from them.
+
+Here’s a short example of how to generate a list of items from an array:
+
+Move the data into an array:
+const people = [
+  'Creola Katherine Johnson: mathematician',
+  'Mario José Molina-Pasquel Henríquez: chemist',
+  'Mohammad Abdus Salam: physicist',
+  'Percy Lavon Julian: chemist',
+  'Subrahmanyan Chandrasekhar: astrophysicist'
+];
+Map the people members into a new array of JSX nodes, listItems:
+const listItems = people.map(person => <li>{person}</li>);
+Return listItems from your component wrapped in a <ul>:
+return <ul>{listItems}</ul>;
  */
