0d2b6bc858920457d9236664cc56931efca295d1
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-19 11:28:43 +0530

    Updated

--- src/Describing the UI/6 - Conditional Rendering.jsx
+++ src/Describing the UI/6 - Conditional Rendering.jsx
@@ -1,4 +1,34 @@
 /*
 Your components will often need to display different things depending on different conditions. In React, you can conditionally render JSX using JavaScript syntax like if statements, &&, and ? : operators.
 
+Conditionally returning JSX 
+Let’s say you have a PackingList component rendering several Items, which can be marked as packed or not:
+
+function Item({ name, isPacked }) {
+  return <li className="item">{name}</li>;
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item 
+          isPacked={true} 
+          name="Space suit" 
+        />
+        <Item 
+          isPacked={true} 
+          name="Helmet with a golden leaf" 
+        />
+        <Item 
+          isPacked={false} 
+          name="Photo of Tam" 
+        />
+      </ul>
+    </section>
+  );
+}
+
+
  */
