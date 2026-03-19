109f59eeafea9a5fc369da2bfa8835512962e53e
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-19 11:28:12 +0530

    Updated

--- src/Describing the UI/6 - Conditional Rendering.jsx
+++ src/Describing the UI/6 - Conditional Rendering.jsx
@@ -1,3 +1,4 @@
 /*
+Your components will often need to display different things depending on different conditions. In React, you can conditionally render JSX using JavaScript syntax like if statements, &&, and ? : operators.
 
  */
