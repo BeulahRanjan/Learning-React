54cf09206047922bbe4328f1244d93f6fdc0d1f6
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-24 15:05:36 +0530

    Updated

--- src/Describing the UI/8 - Keeping Components Pure.jsx
+++ src/Describing the UI/8 - Keeping Components Pure.jsx
@@ -7,5 +7,37 @@ In computer science (and especially the world of functional programming), a pure
 It minds its own business. It does not change any objects or variables that existed before it was called.
 Same inputs, same output. Given the same inputs, a pure function should always return the same result.
 
+React is designed around this concept. React assumes that every component you write is a pure function. This means that React components you write must always return the same JSX given the same inputs:
+
+function Recipe({ drinkers }) {
+  return (
+    <ol>    
+      <li>Boil {drinkers} cups of water.</li>
+      <li>Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.</li>
+      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
+    </ol>
+  );
+}
+
+export default function App() {
+  return (
+    <section>
+      <h1>Spiced Chai Recipe</h1>
+      <h2>For two</h2>
+      <Recipe drinkers={2} />
+      <h2>For a gathering</h2>
+      <Recipe drinkers={4} />
+    </section>
+  );
+}
+
+When you pass drinkers={2} to Recipe, it will return JSX containing 2 cups of water. Always.
+
+If you pass drinkers={4}, it will return JSX containing 4 cups of water. Always.
+
+Just like a math formula.
+
+You could think of your components as recipes: if you follow them and don’t introduce new ingredients during the cooking process, you will get the same dish every time. That “dish” is the JSX that the component serves to React to render.
+
 
  */
