6231c4a194d56368592ace79cd5edc5b5b14dd9d
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-24 15:10:08 +0530

    Updated

--- src/Describing the UI/8 - Keeping Components Pure.jsx
+++ src/Describing the UI/8 - Keeping Components Pure.jsx
@@ -39,5 +39,33 @@ Just like a math formula.
 
 You could think of your components as recipes: if you follow them and don’t introduce new ingredients during the cooking process, you will get the same dish every time. That “dish” is the JSX that the component serves to React to render.
 
+Side Effects: (un)intended consequences
+React’s rendering process must always be pure. Components should only return their JSX, and not change any objects or variables that existed before rendering—that would make them impure!
+let guest = 0;
+
+function Cup() {
+  // Bad: changing a preexisting variable!
+  guest = guest + 1;
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup />
+      <Cup />
+      <Cup />
+    </>
+  );
+}
+
+Output:Tea cup for guest #2
+Tea cup for guest #4
+Tea cup for guest #6
+Bcause In development mode, React (especially with Strict Mode) intentionally calls components twice to detect bugs.
+
+👉 So each <Cup /> runs 2 times
+
+This component is reading and writing a guest variable declared outside of it. This means that calling this component multiple times will produce different JSX! And what’s more, if other components read guest, they will produce different JSX, too, depending on when they were rendered! That’s not predictable.
 
  */
