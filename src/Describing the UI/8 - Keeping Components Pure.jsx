28431172397d02a8dfa6df45d34e2465a552705d
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-24 15:04:09 +0530

    Updated

---
+++ src/Describing the UI/8 - Keeping Components Pure.jsx
@@ -0,0 +1,4 @@
+/*
+Some JavaScript functions are pure. Pure functions only perform a calculation and nothing more. By strictly only writing your components as pure functions, you can avoid an entire class of baffling bugs and unpredictable behavior as your codebase grows. To get these benefits, though, there are a few rules you must follow.
+
+ */
