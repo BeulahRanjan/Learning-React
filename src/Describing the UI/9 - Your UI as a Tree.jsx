9e23ecc3b25f47f59d05de30e3eda76ab9cf948c
Beulah Ranjan <beulahranjan@gmail.com>
2026-03-31 16:25:03 +0530

    Updated

---
+++ src/Describing the UI/9 - Your UI as a Tree.jsx
@@ -0,0 +1,9 @@
+/*
+How does React keep track of your app’s component structure?
+- React, and many other UI libraries, model UI as a tree. Thinking of your app as a tree is useful for understanding the relationship between components. This understanding will help you debug future concepts like performance and state management.
+
+Trees are a relationship model between items. The UI is often represented using tree structures. For example, browsers use tree structures to model HTML (DOM) and CSS (CSSOM). Mobile platforms also use trees to represent their view hierarchy.
+Like browsers and mobile platforms, React also uses tree structures to manage and model the relationship between components in a React app. These trees are useful tools to understand how data flows through a React app and how to optimize rendering and app size.
+
+
+ */
