e713937bb12ed14182f081b1d60baa5591242a0c
Hephzibah Ranjan <beulahranjan@gmail.com>
2026-04-11 12:31:49 +0530

    Updated

---
+++ src/2 - Adding Interactivity/1 - Responding to Events.jsx
@@ -0,0 +1,8 @@
+/*
+React lets you add event Handlers to your JSX. Event handlers are your 
+own functions that will be triggered in response to interactions like 
+clicking,hovering, focusing froem inputs and so on.
+
+
+
+ */
